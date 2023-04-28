28ba65a0169f0fc0e0d2a6ac7149e6c0a6e6e43b
TaviF24 <118764142+TaviF24@users.noreply.github.com>
2023-04-29 00:04:19 +0300

    typo

--- exception_2.cpp
+++ exception_2.cpp
@@ -3,7 +3,7 @@
 //
 #include "exception_2.h"
 
-Prea_mica_suma::Prea_mica_suma() {}a()=default;
+Prea_mica_suma::Prea_mica_suma()=default;
 
 const char *Prea_mica_suma::what() const noexcept {
     return "Ati achitat mai putin decat trebuia\n.";
